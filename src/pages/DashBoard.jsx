767b8cf3cf8ec761f9df2d915c395cc165df661f
seed <yellyhaze000@gmail.com>
2025-02-13 10:59:53 +0800

    updated dashboard v2

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -9,23 +9,43 @@ export default function DashBoard() {
                         <span>Total Accepted RSVP</span>
                     </div>
                     <div className="dash-total">
-                        <strong>531</strong>
+                        <strong>122</strong>
                         <span>864</span>
-                        <span>Total Accepted RSVP</span>
+                        <span>Total Checke-in Guest</span>
                     </div>
                     <div className="dash-total">
-                        <strong>531</strong>
+                        <strong>122</strong>
                         <span>864</span>
-                        <span>Total Accepted RSVP</span>
+                        <span>Total Redeemed Foods</span>
                     </div>
                 </div>
 
-                <div className="admin-status">
-                    <div>
-                        <div>
+                <div className="admin-status text">
+                    <div className="statuc-container">
+                        <div className="status">
                             <span>STEM</span>
                             <span>345</span>
                             <div className="status-bar"><div></div></div>
+                        </div>
+                        <div className="status">
+                            <span>STEM</span>
+                            <span>345</span>
+                            <div className="status-bar"><div></div></div>
+                        </div>
+                        <div className="status">
+                            <span>STEM</span>
+                            <span>345</span>
+                            <div className="status-bar"><div></div></div>
+                        </div>
+                        <div className="status">
+                            <span>STEM</span>
+                            <span>345</span>
+                            <div className="status-bar"><div></div></div>
+                        </div>
+                        <div className="status">
+                            <span>STEM</span>
+                            <span>345</span>
+                            <div className="status-bar"><div className="secondary-bg" style={{width:"50%"}}></div></div>
                         </div>
                     </div>
                     <span>Stand Distribution</span>
